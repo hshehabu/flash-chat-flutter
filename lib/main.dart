7f0f5238837fba17f3f04f9bd047cf2c704c9bf7
hshehabu <hamzashehabu457@gmail.com>
2023-07-27 15:05:58 +0300

    debugShowCheckedModeBanner

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: WelcomeScreen.id,
       routes: {
         RegistrationScreen.id: (context) => RegistrationScreen(),
