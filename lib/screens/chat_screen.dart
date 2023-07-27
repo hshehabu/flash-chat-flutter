a9fdb01cc5edf7cf6fa7ec656817c1e8d5e6ad29
hshehabu <hamzashehabu457@gmail.com>
2023-07-27 17:59:40 +0300

    one way of getting documnets from collection

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,4 @@
-import 'dart:js_interop';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -12,23 +11,33 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final _firestore = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
-//   late User loggedUser;
-// @override
-//   void initState() {
-//     super.initState();
-//     getCurrentUser();
-//   }
-//   void getCurrentUser() async {
-//     try {
-//       final user = _auth.currentUser!;
-//       if (user != null) {
-//         loggedUser = user;
-//       }
-//     } catch (e) {
-//       print(e);
-//     }
-//   }
+  late String messageText;
+  late User loggedUser;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() async {
+    try {
+      final user = await _auth.currentUser!;
+
+      loggedUser = user;
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void getMessages() async {
+    final messages = await _firestore.collection('messages').get();
+    for (var message in messages.docs) {
+      print(message.data());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,14 +68,15 @@ class _ChatScreenState extends State<ChatScreen> {
                   Expanded(
                     child: TextField(
                       onChanged: (value) {
-                        //Do something with the user input.
+                        messageText = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   ElevatedButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      _firestore.collection('messages').add(
+                          {'text': messageText, 'sender': loggedUser.email});
                     },
                     child: Text(
                       'Send',
